7ed1ad0fc11be714b334531029d4b0bfd3aa0656
yqni13 <yqni13@protonmail.com>
2024-05-30 01:48:51 +0200

    refactored; img carousel; about finished

--- js/about_helper.js
+++ js/about_helper.js
@@ -1,4 +1,4 @@
-const aboutGallery = [
+const aboutBox3Gallery = [
     "../assets/gallery/about/about_3.jpg",
     "../assets/gallery/about/about_4.jpg",
     "../assets/gallery/about/about_5.jpg",
@@ -6,27 +6,58 @@ const aboutGallery = [
     "../assets/gallery/about/about_7.jpg",
 ];
 
-const windowImage = "../assets/gallery/about/about_2.jpg";
+const aboutBox4Gallery = [
+    "../assets/gallery/photo4.png",
+    "../assets/gallery/photo16.png",
+    "../assets/gallery/photo21.png",
+];
 
 var galleryIndex = 0;
 var windowIndex = 0;
+var timeoutIDBox4 = 0;
 
 initAboutComponent();
 
 function initAboutComponent() {
     carouselAboutBox3();
-    // carouselAboutBox4();
+    carouselAboutBox4();
 }
 
 function carouselAboutBox3() {
     var bgBox3 = document.getElementById('section_about_box3');
-    if(galleryIndex >= aboutGallery.length) galleryIndex = 0;
+    if(galleryIndex >= aboutBox3Gallery.length) galleryIndex = 0;
 
-    bgBox3.style.backgroundImage = "url(" + aboutGallery[galleryIndex] + ")";
+    bgBox3.style.backgroundImage = "url(" + aboutBox3Gallery[galleryIndex] + ")";
     galleryIndex++;
-    setTimeout(carouselAboutBox3, 3500);
+    setTimeout(carouselAboutBox3, 5000);
 }
 
-// function carouselAboutBox4() {
-//     var image = 
-// }
+function setBox4Img(index) {
+    clearTimeout(timeoutIDBox4);
+    windowIndex = index;
+    carouselAboutBox4();
+}
+
+function carouselAboutBox4() {
+    var windowImg = document.getElementById('about_box4_window');
+    var dots = document.getElementById('about_box4_dot_parent').children;
+    windowImg.innerHTML = '';
+    
+    if(windowIndex >= aboutBox4Gallery.length) windowIndex = 0;
+
+    for(let i = 0; i < aboutBox4Gallery.length; i++) {
+        if(i === windowIndex) {
+            dots[i].style.backgroundColor = 
+            getComputedStyle(document.documentElement).getPropertyValue('--logo-gold');
+        } else {
+            dots[i].style.backgroundColor = 
+            getComputedStyle(document.documentElement).getPropertyValue('--dots-passive');
+        }
+    }
+
+    var img = document.createElement('img');
+    img.src = aboutBox4Gallery[windowIndex];
+    windowImg.append(img);
+    windowIndex++;
+    timeoutIDBox4 = setTimeout(carouselAboutBox4, 5000);
+}
